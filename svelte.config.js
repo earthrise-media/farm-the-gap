1da1b314f9512e14d06edc89526c5dde4bb59001
Brody Smith <62872303+brodysmith1@users.noreply.github.com>
2023-11-16 19:11:16 +1100

    Define fallback

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,9 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: 'index.html'
+		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
