2fcbd7d691d791ec5fd6fb050c385a8c0c531e96
Brody Smith <62872303+brodysmith1@users.noreply.github.com>
2024-02-15 12:40:33 +1100

    Add basepath to server routing

--- src/routes/learn/(posts)/+layout.server.ts
+++ src/routes/learn/(posts)/+layout.server.ts
@@ -1,9 +1,12 @@
+import { base } from "$app/paths"
+
 import articles from "$lib/data/articles"
 
-export async function load({url}) {
-  
+export async function load({ url }) {
+  console.log("URL", url.pathname)
+
   const slug = url.pathname
-  const post = articles.find((post) => slug === `/learn/${post.slug}`)
+  const post = articles.find((post) => slug === `${base}/learn/${post.slug}`)
 
   return { post }
 }
