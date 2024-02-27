13e6b51818789ce22784fb04a45fe77cf92a69ef
Brody Smith <62872303+brodysmith1@users.noreply.github.com>
2024-02-28 08:12:44 +1100

    Update MobileMenu.svelte

--- src/lib/components/MobileMenu.svelte
+++ src/lib/components/MobileMenu.svelte
@@ -7,26 +7,40 @@
 </script>
 
 <div id="mobile-menu" class="text-2xl">
-  <Button bare onClick={() => ($userState.isMenuOpen = !$userState.isMenuOpen)}>
-    <div class="icon icon-menu flex-center bg-primary-2">
-      <Icon type={$userState.isMenuOpen ? "close" : "menu"} />
-    </div>
-  </Button>
-  <Button bare onClick={() => ($userState.isMobileTablesOpen = !$userState.isMobileTablesOpen)}>
-    <div
-      class="icon icon-tables flex-center {$userState.isMobileTablesOpen
-        ? 'bg-tertiary-2 text-primary-0'
-        : 'bg-primary-2'}"
+  <div class="flex-col">
+    <Button bare onClick={() => ($userState.isMenuOpen = !$userState.isMenuOpen)}>
+      <div class="icon icon-menu flex-center bg-primary-2">
+        <Icon type={$userState.isMenuOpen ? "close" : "menu"} />
+      </div>
+    </Button>
+    <Button bare onClick={() => ($userState.isMobileTablesOpen = !$userState.isMobileTablesOpen)}>
+      <div
+        class="icon icon-tables flex-center {$userState.isMobileTablesOpen
+          ? 'bg-tertiary-2 text-primary-0'
+          : 'bg-primary-2'}"
+      >
+        <Icon type="tables" />
+      </div>
+    </Button>
+    <ButtonUndo bare>
+      <div class="icon icon-undo flex-center bg-primary-2">
+        <Icon type="undo" />
+        <span class="label bg-primary-0 food-item-avatar">{$gameState.undosRemaining}</span>
+      </div>
+    </ButtonUndo>
+  </div>
+  <div class="flex-col">
+    <Button
+      bare
+      onClick={() =>
+        ($userState.shareText =
+          "Farm the Gap: The strategy game and learning platform for feeding the future.")}
     >
-      <Icon type="tables" />
-    </div>
-  </Button>
-  <ButtonUndo bare>
-    <div class="icon icon-undo flex-center bg-primary-2">
-      <Icon type="undo" />
-      <span class="label bg-primary-0 food-item-avatar">{$gameState.undosRemaining}</span>
-    </div>
-  </ButtonUndo>
+      <div class="icon icon-share flex-center bg-primary-2">
+        <Icon type="share" />
+      </div>
+    </Button>
+  </div>
 </div>
 
 <style lang="sass">
@@ -39,10 +53,14 @@
   #mobile-menu
     position: relative
     z-index: 1
-    gap: 0.75rem
     display: flex
     align-items: center
+    height: 100%
+    justify-content: space-between
     flex-direction: column
+
+    > .flex-col
+      gap: 0.75rem
 
   .icon
     width: 1.375em
