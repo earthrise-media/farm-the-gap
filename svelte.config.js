aed55660b35ec995a3c1c719bbfa356f3b160e6e
Brody Smith <62872303+brodysmith1@users.noreply.github.com>
2024-03-04 14:33:26 +1100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+console.log("ARGV", process.argv)
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
